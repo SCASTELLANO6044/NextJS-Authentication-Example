812dea965d19a9b3615db08f955a7cae921a9612
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-02-21 22:29:02 +0100

    :sparkles: Capture user credentials and Validate form fields

---
+++ app/actions/auth.ts
@@ -0,0 +1,24 @@
+import { SignupFormSchema, FormState } from '@/app/lib/definitions'
+import bcrypt from 'bcryptjs'
+
+export async function signup(state: FormState, formData: FormData) {
+    // Validate form fields
+    const validatedFields = SignupFormSchema.safeParse({
+        name: formData.get('name'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+    })
+
+    // If any form fields are invalid, return early
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+
+    // 2. Prepare data for insertion into database
+    const { name, email, password } = validatedFields.data
+    // e.g. Hash the user's password before storing it
+    const hashedPassword = await bcrypt.hash(password, 10)
+    // Call the provider or db to create a user...
+}
